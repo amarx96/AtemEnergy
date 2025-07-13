90f665a9d4ac77df4360116212db7aa52201e1d2
Alexander Marx <156967388+amarx96@users.noreply.github.com>
2025-07-13 22:39:55 +0200

    Coding the Constraints

--- Optimizer__Main__.jl
+++ Optimizer__Main__.jl
@@ -17,6 +17,9 @@ technologies = ["SolarPV"]
 storages = ["Battery"]
 fuels = ["Power"]
 
+# And a Dict of maximum capacities in MW
+MaxCapacity = Dict("SolarPV" => 5.0, "Battery" => 5.0)
+
 η_charge = 0.95  # Efficiency of charging
 η_discharge = 0.95  # Efficiency of discharging
 
@@ -42,6 +45,7 @@ m = Model(HiGHS.Optimizer)
 @variable(m, TotalStorageCost[storages,Timestamps] >= 0)
 
 
+### Implement Constraints ###
 # calculate the total cost
 @constraint(m, ProductionCost[t in technologies,τ in Timestamps],
     sum(FuelProductionByTechnology[t,f,τ] for f in fuels, τ in Timestamps) * VariableCost[2020,t] + NewCapacity[t] * InvestmentCost[2020,t] == TotalCost[t]
@@ -57,7 +61,28 @@ m = Model(HiGHS.Optimizer)
     InputRatio[t,f] * sum(FuelProductionByTechnology[y,r,h,t,ff] for ff in fuels) == FuelUseByTechnology[y,r,h,t,f]
 )
 
-# define the emissions
-@constraint(ESM, TechnologyEmissionFunction[y in year,r in regions,t in technologies],
-    sum(FuelProductionByTechnology[y,r,h,t,f] for f in fuels, h in hour) * EmissionRatio[t] == AnnualTechnologyEmissions[y,r,t]
+# installed capacity is limited by the maximum capacity
+@constraint(ESM, MaxCapacityFunction[t in technologies, τ in Timestamps],
+     AccumulatedCapacity[t] <= MaxCapacity[t]
+)
+
+### Add your storage constraints here
+    @constraint(ESM, StorageChargeFunction[y in year,r in regions,s in storages, h in hour, f in fuels; StorageDischargeEfficiency[s,f]>0], 
+    StorageCharge[y,r,s,h,f] <= AccumulatedStorageEnergyCapacity[y,r,s,f]/E2PRatio[s]
+)
+
+@constraint(ESM, StorageDischargeFunction[y in year,r in regions,s in storages, h in hour, f in fuels; StorageDischargeEfficiency[s,f]>0], 
+    StorageDischarge[y,r,s,h,f] <= AccumulatedStorageEnergyCapacity[y,r,s,f]/E2PRatio[s]
+)
+
+@constraint(ESM, StorageLevelFunction[y in year,r in regions,s in storages, h in hour, f in fuels; h>1 && StorageDischargeEfficiency[s,f]>0], 
+    StorageLevel[y,r,s,h,f] == StorageLevel[y,r,s,h-1,f]*StorageLosses[s,f] + StorageCharge[y,r,s,h,f]*StorageChargeEfficiency[s,f] - StorageDischarge[y,r,s,h,f]/StorageDischargeEfficiency[s,f]
+)
+
+@constraint(ESM, StorageLevelStartFunction[y in year,r in regions,s in storages, h in hour, f in fuels; h==1 && StorageDischargeEfficiency[s,f]>0], 
+    StorageLevel[y,r,s,h,f] == 0.5*AccumulatedStorageEnergyCapacity[y,r,s,f]*StorageLosses[s,f] + StorageCharge[y,r,s,h,f]*StorageChargeEfficiency[s,f] - StorageDischarge[y,r,s,h,f]/StorageDischargeEfficiency[s,f]
+)
+
+@constraint(ESM, MaxStorageLevelFunction[y in year,r in regions,s in storages, h in hour, f in fuels; StorageDischargeEfficiency[s,f]>0], 
+    StorageLevel[y,r,s,h,f] <= AccumulatedStorageEnergyCapacity[y,r,s,f]
 )
